755918b8b493cd25ac9f94edb6b5ee38805a1995
hypc <h_yp00@163.com>
2018-03-19 13:02:56 +0800

    update

--- ubuntu-xenial/softs/nvm.sh
+++ ubuntu-xenial/softs/nvm.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-profiles=(~/.bash_profile ~/.zshrc ~/.profile ~/.bashrc)
-for profile in ${profiles[*]}; do
-    if test -f $profile; then
-        break
+
+profile=''
+SHELLTYPE=$(basename $SHELL)
+if [ "$SHELLTYPE" = "bash" ]; then
+    if [ -f "~/.bashrc" ]; then
+        profile="~/.bashrc"
+    elif [ -f "~/.bash_profile" ]; then
+        profile="~/.bash_profile"
     fi
-done
+fi
+if [ -z "$profile" ]; then
+    for profile in "~/.profile" "~/.bashrc" "~/.bash_profile"; do
+        if test -f $profile; then
+            break
+        fi
+    done
+fi
+
 echo "NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node" >> $profile
 echo "NVM_IOJS_ORG_MIRROR=https://npm.taobao.org/mirrors/iojs" >> $profile
 tee ~/.npmrc <<EOF
