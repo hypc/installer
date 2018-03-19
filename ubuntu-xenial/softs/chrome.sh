b0390effda1807c680c3170b53a97aab26ec9a4e
hypc <h_yp00@163.com>
2018-03-19 13:30:04 +0800

    update

--- ubuntu-xenial/softs/chrome.sh
+++ ubuntu-xenial/softs/chrome.sh
@@ -4,4 +4,4 @@ curl -fsSL https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key 
 sudo tee /etc/apt/sources.list.d/google-chrome.list <<EOF
 deb http://dl.google.com/linux/chrome/deb/ stable main
 EOF
-sudo apt-get update && apt-get install -y google-chrome-stable
+sudo apt-get update && sudo apt-get install -y google-chrome-stable
