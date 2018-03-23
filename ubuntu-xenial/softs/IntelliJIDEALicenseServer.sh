abef93cafa015770b93e9689dfabdee53276c5ae
hypc <h_yp00@163.com>
2018-03-23 17:00:42 +0800

    update

--- ubuntu-xenial/softs/IntelliJIDEALicenseServer.sh
+++ ubuntu-xenial/softs/IntelliJIDEALicenseServer.sh
@@ -4,14 +4,14 @@
 # ### This script can only activate JetBrains2017.2 series. ###
 # #############################################################
 
-sudo wget -O /usr/bin/IntelliJIDEALicenseServer https://oe5hf47m4.qnssl.com/IntelliJIDEALicenseServer/2017.2/IntelliJIDEALicenseServer_linux_amd64
-sudo chmod a+x /usr/bin/IntelliJIDEALicenseServer
+sudo wget -O /usr/local/bin/IntelliJIDEALicenseServer https://oe5hf47m4.qnssl.com/IntelliJIDEALicenseServer/2017.2/IntelliJIDEALicenseServer_linux_amd64
+sudo chmod a+x /usr/local/bin/IntelliJIDEALicenseServer
 sudo tee /etc/systemd/system/IntelliJIDEALicenseServer.service <<EOF
 [Unit]
 Description=IntelliJIDEALicenseServer
 
 [Service]
-ExecStart=/usr/bin/IntelliJIDEALicenseServer -p 1234 -u $(basename $HOME)
+ExecStart=/usr/local/bin/IntelliJIDEALicenseServer -p 1234 -u $(basename $HOME)
 EOF
 
 sudo systemctl enable IntelliJIDEALicenseServer.service
