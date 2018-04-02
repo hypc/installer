947c130031537d7db6bc4618e665572b5a1c7126
hypc <h_yp00@163.com>
2018-04-02 11:25:46 +0800

    update nvm

--- ubuntu-xenial/softs/nvm.sh
+++ ubuntu-xenial/softs/nvm.sh
@@ -23,7 +23,10 @@ echo "NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node" >> $profile
 echo "NVM_IOJS_ORG_MIRROR=https://npm.taobao.org/mirrors/iojs" >> $profile
 tee ~/.npmrc <<EOF
 registry=https://registry.npm.taobao.org
-chromedriver_cdnurl=https://npm.taobao.org/mirrors/chromedriver
+
+chromedriver_cdnurl=https://npm.taobao.org/mirrors/chromedriver/
+electron_mirror=https://npm.taobao.org/mirrors/electron/
+phantomjs_cdnurl=https://npm.taobao.org/mirrors/phantomjs/
 sass_binary_site=https://npm.taobao.org/mirrors/node-sass/
 EOF
 source $profile
