f968412ecee58ad121927423000c7e403e87f953
hypc <h_yp00@163.com>
2018-04-08 18:15:00 +0800

    update

--- ubuntu-xenial/softs/nvm.sh
+++ ubuntu-xenial/softs/nvm.sh
@@ -2,25 +2,6 @@
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
-profile=''
-SHELLTYPE=$(basename $SHELL)
-if [ "$SHELLTYPE" = "bash" ]; then
-    if [ -f "~/.bashrc" ]; then
-        profile="~/.bashrc"
-    elif [ -f "~/.bash_profile" ]; then
-        profile="~/.bash_profile"
-    fi
-fi
-if [ -z "$profile" ]; then
-    for profile in "~/.profile" "~/.bashrc" "~/.bash_profile"; do
-        if test -f $profile; then
-            break
-        fi
-    done
-fi
-
-echo "NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node" >> $profile
-echo "NVM_IOJS_ORG_MIRROR=https://npm.taobao.org/mirrors/iojs" >> $profile
 tee ~/.npmrc <<EOF
 registry=https://registry.npm.taobao.org
 
@@ -29,5 +10,8 @@ electron_mirror=https://npm.taobao.org/mirrors/electron/
 phantomjs_cdnurl=https://npm.taobao.org/mirrors/phantomjs/
 sass_binary_site=https://npm.taobao.org/mirrors/node-sass/
 EOF
+profile='~/.bashrc'
+echo "NVM_NODEJS_ORG_MIRROR=https://npm.taobao.org/mirrors/node" >> $profile
+echo "NVM_IOJS_ORG_MIRROR=https://npm.taobao.org/mirrors/iojs" >> $profile
 source $profile
 nvm install --lts
