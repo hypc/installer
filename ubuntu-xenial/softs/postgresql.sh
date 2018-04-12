3c93fdb5d78f84384b5d5afba995f7c154cbf3f6
hypc <h_yp00@163.com>
2018-04-12 17:18:59 +0800

    update

--- ubuntu-xenial/softs/postgresql.sh
+++ ubuntu-xenial/softs/postgresql.sh
@@ -4,4 +4,4 @@ curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add
 sudo tee /etc/apt/sources.list.d/pgdg.list <<EOF
 deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main
 EOF
-sudo apt-get update && sudo apt-get install -y postgresql-9.6
+sudo apt-get update && sudo apt-get install -y postgresql-10 pgadmin4
