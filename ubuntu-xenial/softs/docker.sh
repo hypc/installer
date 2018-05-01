f932f9150b7a23d600e7f2917eb0eab19a5ee0e9
hypc <h_yp00@163.com>
2018-05-01 17:23:23 +0800

    update docker

--- ubuntu-xenial/softs/docker.sh
+++ ubuntu-xenial/softs/docker.sh
@@ -5,3 +5,5 @@ sudo tee /etc/apt/sources.list.d/docker-ce.list <<EOF
 deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu xenial stable
 EOF
 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates software-properties-common docker-ce
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
