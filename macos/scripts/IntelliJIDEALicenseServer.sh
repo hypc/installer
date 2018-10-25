60d530176cb5ac20a92da19d7c562cf091b70d50
hypc <h_yp00@163.com>
2018-10-25 14:20:37 +0800

    update

--- macos/scripts/IntelliJIDEALicenseServer.sh
+++ macos/scripts/IntelliJIDEALicenseServer.sh
@@ -33,3 +33,11 @@ tee ~/Library/LaunchAgents/IntelliJIDEALicenseServer.plist <<EOF
 </plist>
 EOF
 launchctl load -w ~/Library/LaunchAgents/IntelliJIDEALicenseServer.plist
+
+if test -d "/Applications/PyCharm.app"; then
+    echo 'pycharm already installed'
+    exit 0
+fi
+curl -o ~/Downloads/pycharm.dmg https://download.jetbrains.com/python/pycharm-professional-2017.2.7.dmg
+hdiutil attach -quiet ~/Downloads/pycharm.dmg
+cp -r ~/Volumes/PyCharm/PyCharm.app /Applications/
--- macos/scripts/pyenv.sh
+++ macos/scripts/pyenv.sh
@@ -42,11 +42,3 @@ cat <<"EOF">> $PROFILE
 export LDFLAGS="-L/usr/local/opt/sqlite/lib"
 export CPPFLAGS="-I/usr/local/opt/sqlite/include"
 EOF
-
-if test -d "/Applications/PyCharm.app"; then
-    echo 'pycharm already installed'
-    exit 0
-fi
-curl -o ~/Downloads/pycharm.dmg https://download.jetbrains.com/python/pycharm-professional-2017.2.7.dmg
-hdiutil attach -quiet ~/Downloads/pycharm.dmg
-cp -r ~/Volumes/PyCharm/PyCharm.app /Applications/
